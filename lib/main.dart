049bb7dec088590aa2acaf6fa3782b26edec10b4
AdrielP99 <adriel.paixao@soulasalle.com.br>
2025-09-03 13:46:14 -0300

    Statless e Stateful widgets

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,82 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
+        useMaterial3: false,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Meu primeiro app")),
+        body: MeuStatelessWidget(),
+      ),
+    );
+  }
+}
+
+class MeuStatelessWidget extends StatefulWidget {
+  const MeuStatelessWidget({super.key});
+
+  @override
+  State<MeuStatelessWidget> createState() => _MeuStatelessWidgetState();
+}
+
+class _MeuStatelessWidgetState extends State<MeuStatelessWidget> {
+  int count = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Count: $count",
+              style: TextStyle(fontSize: 24, color: Colors.blue),
+            ),
+          ],
+        ),
+        SizedBox(height: 16),
+        ElevatedButton(
+          onPressed: () {
+            setState(() {
+              count++;
+            });
+          },
+          child: Text("Clique Aqui!"),
+        ),
+        MeuStatefulWidget(
+          callback: () {
+            setState(() {
+              count++;
+            });
+          },
+        ),
+      ],
+    );
+  }
+}
+
+class MeuStatefulWidget extends StatefulWidget {
+  final void Function() callback;
+  const MeuStatefulWidget({super.key, required this.callback});
+
+  @override
+  State<MeuStatefulWidget> createState() => _MeuStatefulWidgetState();
+}
+
+class _MeuStatefulWidgetState extends State<MeuStatefulWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {
+            widget.callback();
+          },
+          child: Text("Botão de baixo"),
+        ),
+      ],
     );
   }
 }
